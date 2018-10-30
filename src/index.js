7732fa03f50dfe1d0f9e9a9b5dca0936e7abbcf2
youssef maghzaz <youssefmaghzaz@gmail.com>
2018-10-30 15:32:01 +0100

    add rx-chache

--- src/index.js
+++ src/index.js
@@ -1,22 +1,109 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+const {
+  forkJoin,
+  from,
+  of,
+  combineLatest,
+  BehaviorSubject,
+  Observable
+} = require("rxjs");
+const { map, filter, flatMap, mergeMap } = require("rxjs/operators");
+const generateID = require("./generateID");
+const Background = {
+  blue: "\x1b[44m%s\x1b[0m",
+  yellow: "\x1b[33m%s\x1b[0m",
+  red: "\x1b[41m%s\x1b[0m"
+};
+const store = {};
 
-import styles from './styles.css'
+const notification = {};
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+const getId = (obsId, params) => {
+  return generateID({ obsId, params });
+};
+
+const dispatch = ({ resourceName: obsId, stream: obs, params }) => {
+  const id = getId(obsId, params);
+  if (!store[id]) {
+    store[id] = { stream: obs, called: false };
+    notification[id] = new BehaviorSubject({
+      data: null,
+      error: null,
+      state: "INIT"
+    });
   }
+  const callRessource = () => {
+    if (!store[id].called) {
+      console.log("called : obsId", obsId);
+      store[id].called = true;
+      store[id].unsubscribe = store[id].stream.subscribe(
+        info => {
+          notification[id].next({ data: info, state: "END" });
+        },
+        error => console.log("Error: ", error),
+        () => {
+          console.log("complete");
+        }
+      );
+    }
+  };
+  return {
+    then: call => {
+      callRessource();
+      return new Promise((resolve, reject) => {
+        notification[id].asObservable().subscribe(
+          result => {
+            if (result.state === "END") {
+              resolve(result.data);
+            }
+          },
+          error => {
+            reject(error);
+          }
+        );
+      }).then(call);
+    },
+    subscribe: (call, errorHandler) => {
+      callRessource();
+      return notification[id].subscribe(
+        result => {
+          if (result.error) {
+            return errorHandler(result.error);
+          }
+          if (result.state === "END") {
+            return call(result.data);
+          }
+        },
+        error => {
+          errorHandler(error);
+        }
+      );
+    },
+    unsubscribe: () => {
+      return store[id].unsubscribe.unsubscribe();
+    }
+  };
+};
 
-  render() {
-    const {
-      text
-    } = this.props
+const rxRequest = (partnerFunction, partnerResource, params) => {
+  return new Observable(obs => {
+    console.log(Background.red, "Called ");
+    console.log(Background.yellow, partnerResource);
+    partnerFunction(partnerResource, params, data => {
+      console.log("partnerResource", data);
+      obs.next(data);
+    });
+  });
+};
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
-}
+const buildPartnerRequest = (partnerFunction, resourceName, params) => {
+  return {
+    resourceName,
+    stream: rxRequest(partnerFunction, resourceName, params),
+    params
+  };
+};
+
+module.exports = {
+  buildPartnerRequest,
+  dispatch
+};
